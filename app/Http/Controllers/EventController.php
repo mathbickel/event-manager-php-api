13fe37eb9de4fcd4a91ca9efe92d8891a87f991f
Matheus <matheus.bickel@bagy.com.br>
2025-01-23 19:13:27 -0300

    task: event routes

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use App\Http\Controllers\Controller;
 class EventController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrganizerController;
+use App\Http\Controllers\EventController;
 
 Route::group(['prefix' => 'organizer'], function () {
     Route::get('/all', [OrganizerController::class, 'index'])->name('organizer.index');
@@ -9,4 +10,12 @@ Route::group(['prefix' => 'organizer'], function () {
     Route::post('/', [OrganizerController::class, 'store'])->name('organizer.store');
     Route::put('/{id}', [OrganizerController::class, 'update'])->name('organizer.update');
     Route::delete('/{id}', [OrganizerController::class, 'destroy'])->name('organizer.destroy');
+});
+
+Route::group(['prefix' => 'event'], function () {
+    Route::get('/all', [EventController::class, 'index'])->name('event.index');
+    Route::get('/{id}', [EventController::class, 'getById'])->name('event.show');
+    Route::post('/', [EventController::class, 'store'])->name('event.store');
+    Route::put('/{id}', [EventController::class, 'update'])->name('event.update');
+    Route::delete('/{id}', [EventController::class, 'destroy'])->name('event.destroy');
 });
