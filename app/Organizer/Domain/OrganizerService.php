7126e0102ceac75f20f381a77901e9854c2e8392
Matheus <matheus.bickel@bagy.com.br>
2025-02-18 11:33:26 -0300

    task: .

--- app/Organizer/Domain/OrganizerService.php
+++ app/Organizer/Domain/OrganizerService.php
@@ -6,38 +6,7 @@ use App\Common\Getter;
 use App\Common\Creator;
 use App\Common\Updater;
 use App\Common\Deleter;
-use Illuminate\Database\Eloquent\Collection;
 interface OrganizerService extends Getter, Creator, Updater, Deleter
-{
-    /**
-    * @return Collection
-    */
-    public function getAll(): Collection;
+{  
 
-      /**
-     * @param int $id
-     * @return Organizer
-     */
-    public function getOne(int $id): Organizer;
-
-    /**
-     * @param array $data
-     * @return Organizer
-     */
-
-    public function create(array $data): Organizer;
-    
-    /**
-     * @param int $id
-     * @param array $data
-     * @return Organizer
-     */
-
-    public function update(array $data, int $id): Organizer;
-
-    /**
-     * @param int $id
-     * @return bool 
-     */
-    public function delete(int $id): bool;
 }
