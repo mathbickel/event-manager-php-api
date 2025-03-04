56dd207541dd8b27679213fb36c1311f7e4e6f65
Matheus <matheus.bickel@bagy.com.br>
2025-03-04 13:38:25 -0300

    feat: cache redis

---
+++ app/Common/Cache/CacheRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Common\Cache;
+
+use Exception;
+use Predis\Connection\ConnectionException;
+use Predis\Connection\ServerException;
+use Predis\Connection\ConnectionInterface;
+
+interface CacheRepository
+{
+    /**
+     * @return T
+     * @throws ConnectionException
+     * @throws ServerException
+     */
+    public function getClient();
+
+    /**
+     * @return ConnectionInterface
+     * @throws ConnectionException
+     * @throws ServerException
+     */
+    public function getConnection();
+
+    /**
+     * @param string $key
+     * @return mixed
+     * @throws Exception
+     */
+    public function get(string $key);
+
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param int|null $ttl
+     * @return void
+     * @throws Exception
+     */
+    public function set(string $key, $value, ?int $ttl = null);
+
+    /**
+     * @param string $key
+     * @return void
+     * @throws Exception
+     */
+    public function delete(string $key);
+}
